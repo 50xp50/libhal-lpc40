5826d4c942f3558a6fe2192d2ccd4f18bbee7434
Xander <xanderpereiraj@gmail.com>
2024-07-15 18:15:50 -0700

    Updated sandbox to include different functions for setting and updating LED's

--- demos/applications/sandbox.cpp
+++ demos/applications/sandbox.cpp
@@ -10,6 +10,90 @@
 #include <libhal-util/spi.hpp>
 #include <libhal-util/steady_clock.hpp>
 
+#define LEDAMOUNT 5
+std::array<std::array<hal::byte, 24>, LEDAMOUNT> array_LEDs;
+
+void set_individual_LED(uint8_t LED_number, uint8_t R, uint8_t G, uint8_t B);
+void set_all_LEDs(uint8_t R, uint8_t G, uint8_t B);
+void update_LEDs(hal::lpc40::spi& p_spi);
+
+std::array<std::array<std::uint8_t, 5>, 3> array_colors = {{
+        {255, 255, 0, 0, 160},
+        {0, 255, 255, 0, 32},
+        {0, 0, 0, 255, 240}
+    }};
+
+void application ()
+{
+    using namespace hal::literals;
+
+    hal::cortex_m::dwt_counter steady_clock(
+      hal::lpc40::get_frequency(hal::lpc40::peripheral::cpu));
+
+    std::array<hal::byte, 32> uart_buffer{};
+    hal::lpc40::uart uart0(0, uart_buffer);
+
+    // Need to have data rate 2.5MHz minimum
+    const hal::lpc40::spi::settings spi_settings = {.clock_rate = 2.5_MHz};
+    hal::lpc40::spi spi2(2, spi_settings);
+    hal::lpc40::output_pin chip_select(1, 10);
+    chip_select.level(false);
+
+    hal::print(uart0, "Starting Sandbox Application...\n");
+
+    while (1) {
+      using namespace std::literals;
+
+      for (int i = 0; i < 5; i++) {
+        set_individual_LED(0, array_colors[0][i], array_colors[1][i], array_colors[2][i]);
+        set_individual_LED(1, array_colors[0][(i+1)%5], array_colors[1][(i+1)%5], array_colors[2][(i+1)%5]);
+        set_individual_LED(2, array_colors[0][(i+2)%5], array_colors[1][(i+2)%5], array_colors[2][(i+2)%5]);
+        set_individual_LED(3, array_colors[0][(i+3)%5], array_colors[1][(i+3)%5], array_colors[2][(i+3)%5]);
+        set_individual_LED(4, array_colors[0][(i+4)%5], array_colors[1][(i+4)%5], array_colors[2][(i+4)%5]);
+        update_LEDs(spi2);
+        hal::delay(steady_clock, 100ms);
+      }
+      hal::print(uart0, "Looped\n");
+    }
+}
+
+void set_individual_LED(uint8_t LED_position, uint8_t R, uint8_t G, uint8_t B) {
+  uint32_t color = G << 16 | R << 8 | B;
+
+  int8_t j = 0;
+  for (int8_t k = 23; k >= 0; k--) {
+    if (((color >> k) & 0x01) == 1) {
+      array_LEDs[LED_position][j] = 0xE0;
+    } 
+    else {
+      array_LEDs[LED_position][j] = 0x80;
+    }
+    j++;
+  }
+}
+void set_all_LEDs(uint8_t R, uint8_t G, uint8_t B) {
+  uint32_t color = G << 16 | R << 8 | B;
+
+  for (int8_t i = 0; i < LEDAMOUNT; i++) {
+    int8_t j = 0;
+    for (int8_t k = 23; k >= 0; k--) {
+      if (((color >> k) & 0x01) == 1) {
+        array_LEDs[i][j] = 0xE0;
+      } 
+      else {
+        array_LEDs[i][j] = 0x80;
+      }
+      j++;
+    }
+  }
+}
+
+void update_LEDs(hal::lpc40::spi& p_spi) {
+  for (int i = 0; i < LEDAMOUNT; i++) {
+    hal::write(p_spi, array_LEDs[i]);
+  }
+}
+
 void setLED(uint8_t R, uint8_t G, uint8_t B, hal::lpc40::spi& p_spi, uint8_t r, uint8_t g, uint8_t b) {
 
   uint32_t color = G << 16 | R << 8 | B;
@@ -45,36 +129,6 @@ void setLED(uint8_t R, uint8_t G, uint8_t B, hal::lpc40::spi& p_spi, uint8_t r, 
   hal::write(p_spi, dataLED);
   hal::write(p_spi, dataLED2);
   hal::write(p_spi, dataLED);
-}
-
-void application ()
-{
-    using namespace hal::literals;
-
-    hal::cortex_m::dwt_counter steady_clock(
-      hal::lpc40::get_frequency(hal::lpc40::peripheral::cpu));
-
-    std::array<hal::byte, 32> uart_buffer{};
-    hal::lpc40::uart uart0(0, uart_buffer);
-
-    // Need to have data rate 2.5MHz minimum
-    const hal::lpc40::spi::settings spi_settings = {.clock_rate = 2.5_MHz};
-    hal::lpc40::spi spi2(2, spi_settings);
-    hal::lpc40::output_pin chip_select(1, 10);
-    chip_select.level(false);
-
-    hal::print(uart0, "Starting Sandbox Application...\n");
-
-    while (1) {
-      using namespace std::literals;
-
-      setLED(255, 0, 0, spi2, 0, 255, 0);
-      hal::delay(steady_clock, 1000ms);
-      // setLED(0, 0, 255, spi2, 255, 0, 0);
-      // hal::delay(steady_clock, 1000ms);
-      
-      hal::print(uart0, "Looped\n");
-    }
 }
 
 
